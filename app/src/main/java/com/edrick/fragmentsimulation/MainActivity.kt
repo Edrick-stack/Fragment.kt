5d2ab8c0f3bf00e1bc58cbeca8574dfa68a2964d
Edrick-stack <ecrickcrb02@gmail.com>
2020-10-11 21:20:31 +0700

    Ini fragment

---
+++ app/src/main/java/com/edrick/fragmentsimulation/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.myflexiblefragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mFragmentManager = supportFragmentManager
+        val mHomeFragment = HomeFragment()
+        val fragment = mFragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
+        if (fragment !is HomeFragment)
+        {
+            Log.d("My Fragment", "Nama Fragment :" + HomeFragment::class.java.simpleName)
+            mFragmentManager
+                    .beginTransaction()
+                    .add(R.id.frame_container, mHomeFragment, HomeFragment::class.java.simpleName)
+                    .commit()
+        }
+    }
+}
